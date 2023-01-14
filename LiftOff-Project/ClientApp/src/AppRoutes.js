07a5a0f6a8183066879d35d8dd650ab7adc3cd98
local aknath <aknathcreative@gmail.com>
2023-01-13 22:40:15 -0600

    init

---
+++ LiftOff-Project/ClientApp/src/AppRoutes.js
@@ -0,0 +1,46 @@
+import { Counter } from "./components/Counter";
+import { FetchData } from "./components/FetchData";
+import { Home } from "./components/HomePage/Home";
+import { CreateAccount } from "./components/CreateAccountsPage/CreateAccount";
+import { LogIn } from "./components/LogIn";
+import { SearchResults } from "./components/SearchResultsPage/SearchResults";
+import MyWatchList from "./components/MyWatchList";
+import { UserProfile } from "./components/UserProfile";
+
+const AppRoutes = [
+  {
+    index: true,
+    element: <Home />
+  },
+  {
+    path: '/counter',
+    element: <Counter />
+  },
+  {
+    path: '/fetch-data',
+    element: <FetchData />
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />
+    },
+    {
+        path: '/my-watch-list',
+        element: <MyWatchList />
+    },
+  {
+    path: '/log-in',
+    element: <LogIn />
+  },
+  {
+    path: '/search-results',
+    element: <SearchResults />
+  },
+  {
+    path: '/User-Profile',
+    element: <UserProfile />
+  }
+
+];
+
+export default AppRoutes;
---
+++ LiftOff-Project/ClientApp/src/components/NavMenu.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './Global.css';
+
+export class NavMenu extends Component {
+  static displayName = NavMenu.name;
+
+  constructor (props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar () {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+          <NavbarBrand tag={Link} to="/">NextWatch</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/create-account">Create Account</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/my-watch-list">My Watch List</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/log-in">Log In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/search-results">Search Results</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/User-Profile">User Profile</NavLink>
+              </NavItem>
+            </ul>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
+}
